d2fe6b61b82dc598b841bced229f93f55ed33f65
Ankit Prajapati <Ankit Prajapati@DESKTOP-9HHEQF7>
2023-08-02 11:15:59 +0530

    First commit

--- HelloWorld/src/hello/main/Hello.java
+++ HelloWorld/src/hello/main/Hello.java
@@ -1,11 +1,35 @@
 package hello.main;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		Scanner sr = new Scanner(System.in);
+		int m= sr.nextInt();
+		int nums1 []= new int[m];
+		//System.out.println(arr);
+		/*
+		}*/
+		System.out.println("Enter Size :"+ m);
+		System.out.println("Value :"+ nums1);
+		//Arrays.sort(nums1);
+	/*	for(int sum : nums1 ) {
+			System.out.println(sum);
+		
+		}*/
+		sr.close();
 
+		/*Scanner se = new Scanner(System.in);
+		int m=3,n=3;
+		int num1 [] = new int[m];
+		int num2 [] = new int[n];
+		System.out.println("Num 1: " + num1);
+		System.out.println("Num 1: " +num2);
+		se.close();*/
 	}
 
 }
